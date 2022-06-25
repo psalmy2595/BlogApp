ece47682e25b0788e9c5be84a3379758595e799d
psalmy2595 <samuelj4real17@gmail.com>
2022-06-25 08:06:17 +0100

    Restful Routes and Ejs Templating

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ var express = require("express");
 var app = express ();
 var mongoose = require("mongoose");
 const PORT = process.env.PORT || 9000
-
+ 
+//APP CONFIG
 app.use(express.urlencoded({ extended: true}));
 app.set("view engine", "ejs");
-
 //create db and connect mongodb to app
 mongoose.connect('mongodb://localhost:27017/BlogApp', {useNewUrlParser: true, useUnifiedTopology: true});
 
 
-//Create our schema
+//Mongoose/Model Config
 var blogSchema = mongoose.Schema({
     tittle: String,
     image: String,
@@ -19,13 +19,18 @@ var blogSchema = mongoose.Schema({
     created: {type: Date, default: Date.now}
 });
 //compile campground into a model
-var Blog = mongoose.model("Blog, blogSchema");
+var Blog = mongoose.model("Blog", blogSchema);
 
 
+//RESTFUL ROUTES
 
+app.get("/", function(req, res){
+    res.redirect("/blogs")
+});
 
-
-
+app.get("/blogs", function(req, res){
+    res.render("index")
+});
 
 
 
