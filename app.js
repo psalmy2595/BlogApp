7213b74fe04b21816b5796c42e381681b3965e24
psalmy2595 <samuelj4real17@gmail.com>
2022-07-14 05:37:29 +0100

    Sanitize the user input req.body from form = blog.body in the update route

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ app.get("/blogs/:id/edit", function(req, res){
 //find by id and update and redirect 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     // Blog.findByIdAndUpdate(id, newData, Callback function)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if (err){
